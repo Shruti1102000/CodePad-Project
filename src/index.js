d4b1e345aa2157a79f99dba8c50f825af5de1646
Shriyanshi Dixit <shriyanshidixit2018@gmail.com>
2021-05-17 18:31:04 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import'bootstrap/dist/css/bootstrap.min.css';
+
+import'bootstrap/dist/js/bootstrap.bundle.min';
+
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+import Home from './Home';
+import {
+	BrowserRouter as Router,
+	Route,
+	Link,
+	Switch
+} from 'react-router-dom';
+import App_auth from './components/App_auth';
+ReactDOM.render(
+  <Router>
+       <Switch>
+		      <Route exact path="/" component={Home}/>
+				<Route exact path="/login" component={App_auth}/>
+	    </Switch>
+    </Router>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

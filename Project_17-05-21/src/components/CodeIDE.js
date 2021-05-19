15b2fb95e0815ca5ccb139ad6da38dc4b485f905
Shruti1102000 <73811913+Shruti1102000@users.noreply.github.com>
2021-05-19 19:11:41 +0530

    Add files via upload

---
+++ Project_17-05-21/src/components/CodeIDE.js
@@ -0,0 +1,47 @@
+//copy content of app.js here
+ //import Header from './components/Header'
+ import Sidebar from './Sidebar'
+ //import Editor from './components/Editor'
+ import Form from './Form'
+ //import Form from './components/Form'
+import './App_auth.css';
+ //import Ipop from './components/Ipop'
+ import Button from './Button';
+import { useState } from 'react';
+import db from './firedb';
+
+// Required for side-effects
+require("firebase/firestore");
+const firebase = require("firebase");
+
+const CodeIDE = ({handleLogout,handleaddcodeparent}) =>{
+  const [code,setCode] = useState('');
+  /*const email1=email;
+  console.log(email1);*/
+  const handlegetCode = (code) =>{
+   setCode(code);
+  }
+  const handleaddcode =(fname) =>{
+    handleaddcodeparent(fname,code);
+   
+  }
+   return (
+    <div className="container ">
+        <button onClick={handleLogout}>Logout</button>
+      <div className="row my-row">
+        <div className="col-md-3 my-col ">
+              <img className="class-rounded" id="user-img " src="./code_logo.png" alt="" height='95px' width= '95px'/>
+              <Sidebar addcode={handleaddcode}/>
+             
+        </div>
+
+        <div className="col-md-8 my-col ">
+             <Form getCode={handlegetCode}/>
+        </div>
+
+      </div>
+    </div>
+    
+  );
+}
+export default CodeIDE;
---
+++ Project_17-05-21/src/components/Sidebar.js
@@ -0,0 +1,67 @@
+import Button from './Button';
+import'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import App2 from './App2';
+import {
+	BrowserRouter as Router,
+	
+	Link
+} from 'react-router-dom';
+import { Modal } from 'bootstrap';
+import db from './firedb';
+import Popup from './Popup';
+// Required for side-effects
+require("firebase/firestore");
+const firebase = require("firebase");
+const Sidebar = ({addcode}) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [fname , setfname] =useState('');
+ 
+    const togglePopup = () => {
+      setIsOpen(!isOpen);
+    }
+   
+    const onClick1 =() =>{
+        /*console.log(code);
+        console.log(email);*/
+        addcode(fname);
+        
+        togglePopup();
+
+
+    }
+
+    return (
+        <div className='sidebar'>
+
+            {/* <input value="User" width="10" className="btn" /> */}
+            <img className="class-rounded" src="./def-user.png" height="95px" width="95px" id="user-img"/>
+            <Button color='' text='View' onClick={onClick1}/>
+            <Button color='' text='Save' onClick={onClick1}/>
+            {isOpen && <Popup
+      content={<>
+        <b>Enter Name of the file</b>
+        <input type="text" value={fname} onChange={(e) => {setfname(e.target.value)}}/>
+        <button className="btn btn-outline-primary" onClick={onClick1}>Save</button>
+      </>}
+      handleClose={togglePopup}
+    />}
+            {/*<Button color='' text='Collaborate'/>*/}
+            <Link to='/App2' className="btn-lg btn-dark btn">Collaborate</Link>
+            <Button color='' text='Share' onClick={onClick1}/>
+            {/* <Button color='' text='Logout' onClick={onClick}/> */}
+           { /*<Modal show={show} onHide={onClick1}>
+                <Modal.Header>Enter Nmae of file</Modal.Header>
+                <Modal.Body>
+                    <input type="text" value="File Name" />
+                </Modal.Body>
+                <Modal.Footer>
+                    <button className="btn btn-outline-danger" onClick={onClick1}>Close</button>
+                    <button className="btn btn-outline-primary" onClick={onClick1}>Save</button>
+                </Modal.Footer>
+      </Modal>*/}
+        </div>
+    )
+}
+
+export default Sidebar
